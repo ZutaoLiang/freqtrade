a04d473be8bc71292f49e82d57e2659fe53b3d52
lzt <liangzutao@gmail.com>
2025-03-02 18:11:03 +0800

    update download helper script

--- download-backtest-data.sh
+++ download-backtest-data.sh
@@ -5,4 +5,4 @@ echo "Backtesting $strategy on timeframe:$timeframe in timerange:$timerange"
 
 whitelist=$(cat config.json | tr -d '\n' | grep -oP '(?<="pair_whitelist": \[)[^\]]*' | tr -d '"' | tr ',' ' ')
 
-./run.sh download-data --timeframe $timeframe --timerange $timerange --pairs $whitelist
+./run.sh download-data --timeframe $timeframe --timerange $timerange --pairs $whitelist --erase
