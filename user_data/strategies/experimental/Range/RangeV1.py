f876878901ab70b150b1390a404f2d165e74f398
lzt <liangzutao@gmail.com>
2026-02-28 17:29:09 +0800

    add RangeV1

---
+++ user_data/strategies/experimental/Range/RangeV1.py
@@ -0,0 +1,365 @@
+import json
+from typing import Optional
+
+import numpy as np
+import pandas_ta as pta
+import pandas as pd
+# pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+from pandas import DataFrame
+
+from freqtrade.constants import Config
+from freqtrade.strategy.interface import IStrategy
+from freqtrade.strategy.strategy_helper import stoploss_from_absolute, stoploss_from_open
+from freqtrade.persistence import Order, Trade
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+from datetime import datetime, timezone, timedelta
+import logging
+logger = logging.getLogger(__name__)
+
+
+class RangeV1(IStrategy):
+
+    timeframe = '5m'
+    
+    minimal_roi = {"0": 100}
+    can_short = True
+    process_only_new_candles = True
+    
+    def __init__(self, config: Config) -> None:
+        super().__init__(config)
+        self.stake_amount = self.get_config("stake_amount", 6)
+        self.trade_leverage = self.get_config("trade_leverage", 3)
+        
+        self.trailing_stop = self.get_config("trailing_stop", True)
+        self.trailing_stop_positive = self.get_config("base_trailing_stop", 0.12) * self.trade_leverage
+        self.trailing_stop_positive_offset = self.get_config("base_trailing_stop_offset", 0.3) * self.trade_leverage
+        self.trailing_only_offset_is_reached = self.get_config("trailing_only_offset_is_reached", True)
+
+        self.base_stop_loss = self.get_config("base_stop_loss", 0.07)
+        self.stoploss = - float(self.base_stop_loss * self.trade_leverage)
+        self.use_custom_stoploss = self.get_config("use_custom_stoploss", False)
+        self.atr_stop_loss_multiplier = self.get_config("atr_stop_loss_multiplier", 0)
+        
+        self.use_ha_candles = self.get_config("use_ha_candles", False)
+        
+        self.trend_length = self.get_config("trend_length", 3)
+ 
+        self.ma_short_length = self.get_config("ma_short_length", 0)
+        self.ma_mid_length = self.get_config("ma_mid_length", 0)
+        self.ma_long_length = self.get_config("ma_long_length", 0)
+
+        self.bb_length = self.get_config("bb_length", 20)
+        self.bb_std = self.get_config("bb_std", 2.0)
+
+        self.startup_candle_count = int(max(self.ma_mid_length, self.ma_long_length) * 1)
+        
+        self.atr_period = self.get_config("atr_period", 21)
+        
+        self.position_adjustment_enable = self.get_config("position_adjustment_enable", False)
+        self.addition_stake_ratio = self.get_config("addition_stake_ratio", 0.8)
+        self.addition_min_profit = self.get_config("addition_min_profit", 0.2)
+        self.addition_min_profit_step = self.get_config("addition_min_profit_step", 0.05)
+        self.addition_profit_step = self.get_config("addition_profit_step", 0.05)
+ 
+        self.cooldown_candles = self.get_config("cooldown_candles", 1)
+        self.stoploss_guard_lookback_period_candles = self.get_config("stoploss_guard_lookback_period_candles", 0)  # 0 is disabled
+        self.stoploss_guard_trade_limit = self.get_config("stoploss_guard_trade_limit", 4)
+        self.stoploss_guard_stop_duration_candles = self.get_config("stoploss_guard_stop_duration_candles", 2)
+        self.max_drawdown_lookback_period = self.get_config("max_drawdown_lookback_period", 0)  # 0 is disabled
+        self.max_drawdown_stop_duration = self.get_config("max_drawdown_stop_duration", 60)
+        self.max_allowed_drawdown = self.get_config("max_allowed_drawdown", 0.3)
+     
+    def get_config(self, key: str, default):
+        return self.config.get(key, default)
+
+    def calculate_ha(self, df: DataFrame) -> DataFrame:
+        if self.use_ha_candles:
+            df_ref = qtpylib.heikinashi(df)
+        else:
+            df_ref = df
+        
+        df['ha_open'] = df_ref['open']
+        df['ha_high'] = df_ref['high']
+        df['ha_low'] = df_ref['low']
+        df['ha_close'] = df_ref['close']
+        return df
+ 
+    def calc_ma(self, close, length: int):
+        # ma = pta.ema(close=close, length=length, talib=False)
+        ma = pta.wma(close=close, length=length, talib=False)
+        return ma.ffill() if ma is not None else ma
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        try:
+            dataframe = self.calculate_ha(dataframe)
+             
+            # ma
+            if self.ma_short_length > 0:
+                dataframe['ma_short'] = self.calc_ma(close=dataframe['ha_close'], length=self.ma_short_length)
+            
+            if self.ma_mid_length > 0:
+                dataframe['ma_mid'] = self.calc_ma(close=dataframe['ha_close'], length=self.ma_mid_length)
+            
+            if self.ma_long_length > 0:
+                dataframe['ma_long'] = self.calc_ma(close=dataframe['ha_close'], length=self.ma_long_length)
+            
+            # atr
+            dataframe['atr'] = pta.atr(dataframe['ha_high'], dataframe['ha_low'], dataframe['ha_close'], length=self.atr_period)
+            dataframe['natr'] = pta.natr(high=dataframe['ha_high'], low=dataframe['ha_low'], close=dataframe['ha_close'], length=self.atr_period, talib=False, scalar=1.0)
+            
+            # boll band
+            bbands = pta.bbands(close=dataframe['ha_close'], length=self.bb_length, std=2.0, mamode='ema', talib=False)
+            dataframe['bb_upper'] = bbands[f"BBB_{self.bb_length}_{self.bb_std}"]
+            dataframe['bb_bandwidth'] = bbands[f"BBB_{self.bb_length}_{self.bb_std}"]
+            
+            return dataframe
+        except Exception as e:
+            logger.error(f"Error in {self.__class__.__name__}::populate_indicators: {e}")
+            return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['enter_long'] = 0
+        dataframe['enter_short'] = 0
+        
+        if dataframe.empty:
+            return dataframe
+        
+        try:
+            enter_long_mask = \
+                (dataframe['ha_close'] >= dataframe['ma_short']) \
+                & (dataframe['ha_close'] >= dataframe['ma_mid']) \
+                & (dataframe['ma_short'] > dataframe['ma_mid']) \
+                & (dataframe['ma_short'].shift(3) < dataframe['ma_mid']).shift(3) \
+                & (self.indicator_up_n_periods_mask(dataframe, 'ma_short', self.trend_length))
+           
+            dataframe.loc[enter_long_mask, ['enter_long', 'enter_tag']] = (1, 'entry_long')
+
+            enter_short_mask = \
+                (dataframe['ha_close'] <= dataframe['ma_short']) \
+                & (dataframe['ha_close'] <= dataframe['ma_mid']) \
+                & (dataframe['ma_short'] <= dataframe['ma_mid']) \
+                & (dataframe['ma_short'].shift(3) >= dataframe['ma_mid']).shift(3) \
+                & (self.indicator_down_n_periods_mask(dataframe, 'ma_short', self.trend_length))
+                    
+            dataframe.loc[enter_short_mask, ['enter_short', 'enter_tag']] = (1, 'entry_short')                    
+            return dataframe
+        except Exception as e:
+            logger.error(f"Error in {self.__class__.__name__}::populate_entry_trend: {e}")
+            return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['exit_long'] = 0
+        dataframe['exit_short'] = 0
+        
+        if dataframe.empty:
+            return dataframe
+        
+        try:
+            dataframe.loc[(
+                    (dataframe['exit_long'] == 0)
+                    & (dataframe['ma_short'] < dataframe['ma_mid'])
+                ), ['exit_long', 'exit_tag']] = (1, 'exit_ma')
+            
+            dataframe.loc[(
+                    (dataframe['exit_short'] == 0)
+                    & (dataframe['ma_short'] > dataframe['ma_mid'])
+                ), ['exit_short', 'exit_tag']] = (1, 'exit_ma')
+            
+            return dataframe
+        except Exception as e:
+            logger.error(f"Error in {self.__class__.__name__}::populate_exit_trend: {e}")
+            return dataframe
+    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime,
+                       current_rate: float, current_profit: float, after_fill: bool,
+                       **kwargs) -> Optional[float]:
+        if self.atr_stop_loss_multiplier <= 0:
+            return None
+        
+        leverage = trade.leverage
+        is_short = trade.is_short
+        open_rate = trade.open_rate
+        _current_profit = current_profit / leverage
+
+        if after_fill:
+            filled_orders = trade.select_filled_orders()
+            count_of_orders = len(filled_orders)
+            if count_of_orders == 0:
+                return None
+            
+            # last_filled_price = filled_orders[-1].average
+            last_candle = self.get_last_candle(trade)
+            atr = last_candle['atr']
+            natr = last_candle['natr']
+            
+            if is_short:
+                stop_rate_atr = open_rate + (self.atr_stop_loss_multiplier * atr) 
+                stop_rate_abs = open_rate * (1 + self.base_stop_loss)
+                stop_rate = min(stop_rate_atr, stop_rate_abs)
+            else:
+                stop_rate_atr = open_rate - (self.atr_stop_loss_multiplier * atr)
+                stop_rate_abs = open_rate * (1 - self.base_stop_loss)
+                stop_rate = max(stop_rate_atr, stop_rate_abs)
+            
+            if count_of_orders == 1:
+                logger.info(f'Set {trade.pair} after fill #{count_of_orders} stoploss rate to:{stop_rate:.6f}'
+                            f'(stop_rate_atr:{stop_rate_atr:.6f}, stop_rate_abs:{stop_rate_abs:.6f}), '
+                            f'[new_open_rate:{open_rate:.6f}](stop/open dist:{abs(stop_rate/open_rate-1):.2%}, atr:{atr:.6f}, natr:{natr:.2%})'
+                            f'current_rate:{current_rate:.6f}, '
+                            f'current_profit:{current_profit:.2%}(without leverage:{_current_profit:.2%}) at {current_time}')
+            else:
+                logger.info(f'Set {trade.pair} after fill #{count_of_orders} stoploss rate to:{stop_rate:.6f}'
+                            f'(stop_rate_atr:{stop_rate_atr:.6f}, stop_rate_abs:{stop_rate_abs:.6f}), '
+                            f'[new_open_rate:{open_rate:.6f}](stop/open dist:{abs(stop_rate/open_rate-1):.2%}, atr:{atr:.6f}, natr:{natr:.2%})'
+                            f'current_rate:{current_rate:.6f}, '
+                            f'current_profit:{current_profit:.2%}(without leverage:{_current_profit:.2%}) at {current_time}')
+            return stoploss_from_absolute(stop_rate, current_rate, is_short, leverage)
+ 
+        return None
+ 
+    def adjust_trade_position(
+        self,
+        trade: Trade,
+        current_time: datetime,
+        current_rate: float,
+        current_profit: float,
+        min_stake: float | None,
+        max_stake: float,
+        current_entry_rate: float,
+        current_exit_rate: float,
+        current_entry_profit: float,
+        current_exit_profit: float,
+        **kwargs,
+    ) -> float | None | tuple[float | None, str | None]:
+        if not self.position_adjustment_enable:
+            return None
+        
+        has_open_orders = any(order.status == "open" and not order.ft_is_open and not order.ft_order_side == 'stoploss' for order in trade.orders)
+        if has_open_orders:
+            logger.info(f'There are open orders for {trade.pair}, skip position adjustment.')
+            return None
+        
+        filled_orders = trade.select_filled_orders()
+        count_of_orders = len(filled_orders)
+        if count_of_orders == 0:
+            logger.info(f'No filled orders for {trade.pair}, skip position adjustment.')
+            return None
+        
+        entry_side_orders = [order for order in filled_orders \
+            if order.ft_order_side == trade.entry_side and ('entry' in order.ft_order_tag)]
+        count_of_orders = len(entry_side_orders)
+        if count_of_orders == 0:
+            logger.info(f'No entry orders for {trade.pair}, skip position adjustment.')
+            return None
+         
+        leverage = trade.leverage
+        _current_profit = current_profit / leverage
+        
+        first_entry_order = entry_side_orders[0]
+        first_stake_amount = first_entry_order.stake_amount * leverage
+        
+        addition_stake = first_stake_amount * self.addition_stake_ratio
+        addition_amount = round(addition_stake / current_rate, 2)
+        if addition_amount <= 0:
+            logger.info(f'Addition amount for {trade.pair} is zero, skip position adjustment.')
+            return None
+        
+        addition_stake = addition_amount * current_rate
+        
+        is_short = trade.is_short
+        factor = -1 if is_short else 1
+        
+        new_open_rate = (trade.amount * trade.open_rate + addition_stake) / (trade.amount + addition_amount)
+        new_open_profit = factor * (current_rate / new_open_rate - 1)
+        
+        enough_profit = new_open_profit > (self.addition_min_profit + self.addition_min_profit_step * (count_of_orders-1))
+        last_entry_price = entry_side_orders[-1].average
+        
+        addition_signal = False
+        if enough_profit:
+            if is_short: # and last_candle['enter_short'] == 1
+                addition_signal = current_rate < last_entry_price * (1 + factor * self.addition_profit_step)
+            elif not is_short: # and last_candle['enter_long'] == 1
+                addition_signal = current_rate > last_entry_price * (1 + factor * self.addition_profit_step)
+
+        if addition_signal:
+            if min_stake <= addition_stake <= max_stake:
+                logger.warning(f'Position addition #{count_of_orders+1} for {trade.pair} with estimated new_profit:{new_open_profit*leverage:.2%}'
+                               f'(without leverage:{new_open_profit:.2%}) and stake amount {addition_stake:.5f}, '
+                               f'current_profit:{current_profit:.2%}, current_rate:{current_rate:.5f}, '
+                               f'open_rate:{trade.open_rate:.5f} at {current_time}')
+                return (addition_stake / leverage, f'entry-addition')
+            else:
+                logger.warning(f'Skip position addition for {trade.pair} with estimated new_profit:{new_open_profit*leverage:.2%}'
+                               f'(without leverage:{new_open_profit:.2%}) and stake amount {addition_stake:.5f} is out of range'
+                               f'({min_stake:.2f}-{max_stake:.2f}), current_profit:{current_profit:.2%}, current_rate:{current_rate:.5f}, '
+                               f'open_rate:{trade.open_rate:.5f} at {current_time}')
+        
+        return None
+ 
+    def get_last_candle(self, trade: Trade):
+        dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
+        last_candle = dataframe.iloc[-1].squeeze()
+        return last_candle
+ 
+    def indicator_up_n_periods_mask(self, dataframe: DataFrame, indicator: str, days: int):
+        indicator_up_mask = (dataframe[f'{indicator}'] > dataframe[f'{indicator}'].shift(1))
+        for i in range(2, days):
+            indicator_up_mask = indicator_up_mask & (dataframe[f'{indicator}'].shift(i-1) > dataframe[f'{indicator}'].shift(i))
+        return indicator_up_mask
+    
+    def indicator_down_n_periods_mask(self, dataframe: DataFrame, indicator: str, days: int):
+        indicator_down_mask = (dataframe[f'{indicator}'] < dataframe[f'{indicator}'].shift(1))
+        for i in range(2, days):
+            indicator_down_mask = indicator_down_mask & (dataframe[f'{indicator}'].shift(i-1) < dataframe[f'{indicator}'].shift(i))
+        return indicator_down_mask
+
+    def leverage(
+        self, 
+        pair: str, 
+        current_time: datetime, 
+        current_rate: float, 
+        proposed_leverage: float, 
+        max_leverage: float, 
+        entry_tag: Optional[str], 
+        side: str, 
+        **kwargs
+    ) -> float:
+        return self.trade_leverage
+
+    @property
+    def protections(self): # type: ignore
+        protections = []
+        
+        if self.cooldown_candles > 0:
+            protections.append(
+                {
+                    "method": "CooldownPeriod",
+                    "stop_duration_candles": self.cooldown_candles,
+                }
+            )
+        
+        if self.stoploss_guard_lookback_period_candles > 0:
+            protections.append(
+                {
+                    "method": "StoplossGuard",
+                    "lookback_period_candles": self.stoploss_guard_lookback_period_candles,
+                    "trade_limit": self.stoploss_guard_trade_limit,
+                    "stop_duration_candles": self.stoploss_guard_stop_duration_candles,
+                    "only_per_pair": False
+                }
+            )
+        
+        if self.max_drawdown_lookback_period > 0:
+            protections.append(
+                {
+                    "method": "MaxDrawdown",
+                    "lookback_period": self.max_drawdown_lookback_period,
+                    "stop_duration": self.max_drawdown_stop_duration,
+                    "max_allowed_drawdown": self.max_allowed_drawdown,
+                }
+            )
+        
+        return protections
+  
